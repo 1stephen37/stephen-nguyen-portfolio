7d8995cdc4feefa0057ec344aec50c076105866d
1stephen37 <ngamingyahoo@gmail.com>
2025-06-10 18:26:59 +0700

    1

--- app/[locale]/work/page.tsx
+++ app/[locale]/work/page.tsx
@@ -1,6 +1,100 @@
+'use client';
+
+import {motion} from 'framer-motion';
+import React, {useState} from "react";
+
+import {Swiper} from 'swiper';
+import "swiper/css";
+
+import {BsArrowRight, BsGithub} from "react-icons/bs";
+
+import {Tooltip, TooltipTrigger, TooltipContent, TooltipProvider} from "@/components/ui/tooltip";
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+const projects = [
+    {
+        num: "01",
+        category: "frontend",
+        title: "project 1",
+        description: "Project 1 description",
+        stack: [
+            {
+                name: "Html 5"
+            },
+            {
+                name: "Css 3"
+            },
+            {
+                name: "JavaScript"
+            }
+        ],
+        image: '/assets/work/thumb1.png',
+        live: "",
+        github: ""
+    },
+    {
+        num: "02",
+        category: "fullstack",
+        title: "project 2",
+        description: "Project 2 description",
+        stack: [
+            {
+                name: "Next.js"
+            },
+            {
+                name: "Tailwind.css"
+            },
+            {
+                name: "Node.js"
+            }
+        ],
+        image: '/assets/work/thumb2.png',
+        live: "",
+        github: ""
+    },
+]
 
 export default function Page() {
+
+    const [project, setProject] = useState(projects[0]);
+
     return (
-        <div>work</div>
+        <motion.section initial={{
+            opacity: 0,
+        }} animate={{
+            opacity: 1,
+        }} className={'min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'}>
+            <div className="container">
+                <div className="flex flex-col xl:flex-row xl:gap-[30px] ">
+                    <div
+                        className="w-full xl:w-1/2 xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+                        <div className="">
+                            <div
+                                className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
+                            <h2 className={`text-[42px] font-bold leading-none
+                             text-white group-hover:text-accent transition-all duration-500 capitalize`}>{project.category} project</h2>
+                            <p className="text-white/60">{project.description}</p>
+                            <ul className={'flex gap-4'}>
+                                {project.stack.map((item, index) => (
+                                    <li key={index} className={'text-xl text-accent'}>{item.name},</li>
+                                ))}
+                            </ul>
+                            <div className="">
+                                <Link href={''}>
+                                    <TooltipProvider>
+                                        <Tooltip>
+
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full">slider</div>
+                </div>
+            </div>
+        </motion.section>
     );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,22 +6,27 @@ import {usePathname} from "next/navigation";
 const links = [
     {
         name: 'home',
+        vName: "trang chủ",
         path: '/',
     },
     {
         name: 'services',
+        vName: "dịch vụ",
         path: '/services',
     },
     {
         name: 'resume',
+        vName: "hồ sơ năng lực",
         path: '/resume',
     },
     {
         name: 'work',
+        vName: "dự án",
         path: '/work',
     },
     {
         name: 'contact',
+        vName: "liên hệ",
         path: '/contact',
     }
 ]
@@ -29,12 +34,16 @@ const links = [
 export default function Nav() {
     const pathname = usePathname();
 
+    const parts = pathname.split('/'); // ['', 'vi', 'resume']
+    const locale = parts[1] || 'vi'; // fallback nếu rỗng
+    const subPath = '/' + parts.slice(2).join('/');
+
     return (
         <nav className={'flex gap-8 items-center'}>
             {links.map((link, index) => (
                 <Link key={index} href={link.path}
-                      className={`${link.path === pathname && 'text-accent border-b-2 border-accent'} capitalize
-                       font-medium hover:text-accent transition-all `}>{link.name}</Link>
+                      className={`${link.path === subPath && 'text-accent border-b-2 border-accent'} capitalize
+                       font-medium hover:text-accent transition-all `}> {locale === 'vi' ? link.vName : link.name}</Link>
             ))}
         </nav>
     );
